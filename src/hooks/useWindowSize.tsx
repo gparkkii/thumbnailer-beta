73d1ac99029fa96535838e877c78490874c74895
gparkkii <jyp933@gmail.com>
2023-05-23 15:38:15 +0900

    feat: implement useWindowSize hooks

---
+++ src/hooks/useWindowSize.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+export default function useWindowSize() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+}
